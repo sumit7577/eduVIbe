3077d0ee91bb45c83c9251cbd3e06e78c80c8ac1
sumit7577 <asmitkumarr082@gmail.com>
2023-12-31 13:45:07 +0530

    updted output mode

--- next.config.js
+++ next.config.js
@@ -1,18 +1,19 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    images: {
-      domains: ["localhost","127.0.0.1"],
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "api.majoringlearn.com",
-          port: "",
-        },
-      ],
-    },
-  };
-  
-  module.exports = nextConfig;
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ["localhost", "127.0.0.1"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.majoringlearn.com",
+        port: "",
+      },
+    ],
+  },
+  output:"standalone"
+};
+
+module.exports = nextConfig;
